4322a8cc4bbaa9a8b68f57cad09875c591ddd0e6
HC <lilan1990917@gmail.com>
2016-06-21 19:48:02 +0800

    optimization

--- src/frame/main.jsx
+++ src/frame/main.jsx
@@ -35,13 +35,7 @@ class Frame extends React.Component {
                 <div className="ant-layout-topaside">
                     <div className="ant-layout-header">
                         <div className="ant-layout-wrapper">
-                            <div className="ant-layout-logo"></div>
-                            <Menu theme="dark" mode="horizontal"
-                                  defaultSelectedKeys={['2']} style={{lineHeight: '64px'}}>
-                                <Menu.Item key="1">导航一</Menu.Item>
-                                <Menu.Item key="2">导航二</Menu.Item>
-                                <Menu.Item key="3">导航三</Menu.Item>
-                            </Menu>
+                            <div className="ant-layout-logo">Homer</div>
                         </div>
                     </div>
                     <div className="ant-layout-wrapper">
--- src/tabs/main.jsx
+++ src/tabs/main.jsx
@@ -10,10 +10,14 @@ class TabPanes extends React.Component{
 	render() {
 		console.log(this.props.selectKey);
 		return (
-			<Tabs activeKey={this.props.selectKey || '0'} onTabClick={this.props.tabClick}>
-			    <TabPane tab="Home" key="0">选项卡一</TabPane>
-			    <TabPane tab="商户录入" key="1">选项卡二</TabPane>
-			    <TabPane tab="商户列表" key="2">选项卡三</TabPane>
+			<Tabs 
+				activeKey={this.props.selectKey || '0'} 
+				onTabClick={this.props.tabClick} 
+				type="editable-card"
+				hideAdd>
+			    	<TabPane tab={<span id="home-tab">Home</span>} key="0">选项卡一</TabPane>
+			    	<TabPane tab="商户录入" key="1">选项卡二</TabPane>
+			    	<TabPane tab="商户列表" key="2">选项卡三</TabPane>
 			</Tabs>
 		);
 	}
